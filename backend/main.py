3de4104ae3dd2018c438caee86694ad6dc8dc2a3
DrewAMSD <drewalgismiller2@gmail.com>
2026-01-12 23:43:40 -0800

    making and getting exercises

--- backend/main.py
+++ backend/main.py
@@ -153,25 +153,124 @@ def create_user(user: CreateUser, conn: Connection = Depends(get_db)):
     return { "username": user.username }
 
 
-
 @app.get("/users/me")
 async def read_users_me(
     current_user: Annotated[User, Depends(get_current_active_user)]
     ):
     return current_user
 
+
+def muscle_in_db(cursor: Cursor, muscle: str):
+    cursor.execute("SELECT * FROM muscles WHERE name = ?", (muscle,))
+    return cursor.fetchone() is not None
+
+
+def insert_exercise_muscles_row(cursor: Cursor, muscle: str, exercise_id: int, is_primary_muscle: bool):
+    cursor.execute("SELECT * FROM muscles WHERE name = ?", (muscle,))
+    row = cursor.fetchone()
+    muscle_id: int = row["id"]
+    cursor.execute("INSERT INTO exercise_muscles (exercise_id, muscle_id, is_primary_muscle) VALUES (?, ?, ?)",
+                   (exercise_id, muscle_id, is_primary_muscle))
+
+
 @app.post("/users/me/exercises", status_code=status.HTTP_201_CREATED)
-async def create_exercise(
+def create_exercise(
     exercise: Exercise, 
     current_user: Annotated[User, Depends(get_current_active_user)],
-    conn = Annotated[Connection, Depends(get_db)]
+    conn: Connection = Depends(get_db)
     ):
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM exercises WHERE name = ? and username = ?",(exercise.name, current_user.username))
     if cursor.fetchone():
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Exercise already exists")
     
-    # unfinished here
+    for primary_muscle in exercise.primary_muscles:
+        if not muscle_in_db(cursor, primary_muscle):
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"Muscle '{primary_muscle}' does not exist")
+    if exercise.secondary_muscles is not None:
+        for secondary_muscle in exercise.secondary_muscles:
+            if not muscle_in_db(cursor, secondary_muscle):
+                raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"Muscle '{secondary_muscle}' does not exist")
+            
+    cursor.execute("INSERT INTO exercises (name, username, description, weight, reps, time) VALUES (?, ?, ?, ?, ?, ?)",
+                   (exercise.name, current_user.username, exercise.description, exercise.weight, exercise.reps, exercise.time))
+    exercise_id: int = cursor.lastrowid
+    for primary_muscle in exercise.primary_muscles:
+        insert_exercise_muscles_row(cursor, primary_muscle, exercise_id, True)
+    if exercise.secondary_muscles is not None:
+        for secondary_muscle in exercise.secondary_muscles:
+            insert_exercise_muscles_row(cursor, secondary_muscle, exercise_id, False)
+
+    conn.commit()
+    return exercise
+
+
+def get_exercise_muscles(cursor: Cursor, exercise_id: int, is_primary: bool):
+    cursor.execute("SELECT * FROM exercise_muscles WHERE exercise_id = ? AND is_primary_muscle = ?", (exercise_id, is_primary))
+    exercise_muscles_rows = cursor.fetchall()
+    if not exercise_muscles_rows:
+        return None
+    muscles: list[str] = []
+    for exercise_muscles_row in exercise_muscles_rows:
+        cursor.execute("SELECT * FROM muscles where id = ?", (exercise_muscles_row["muscle_id"],))
+        muscles_row: sqlite3.Row = cursor.fetchone()
+        muscles.append(muscles_row["name"])
+    return muscles
+
+
+def convert_exercises_row_to_exercise(cursor: Cursor, exercises_row: sqlite3.Row):
+    primary_muscles: list[str] = get_exercise_muscles(cursor, exercises_row["id"], True)
+    secondary_muscles: list[str] = get_exercise_muscles(cursor, exercises_row["id"], False)
+
+    return {
+        "name": exercises_row["name"],
+        "username": exercises_row["username"],
+        "primary_muscles": primary_muscles,
+        "secondary_muscles": secondary_muscles,
+        "description": exercises_row["description"],
+        "weight": exercises_row["weight"],
+        "reps": exercises_row["reps"],
+        "time": exercises_row["time"]
+    }
+
+
+def get_exercises(cursor: Cursor, username: str = None):
+    if username is None:
+        cursor.execute("SELECT * FROM exercises WHERE username IS NULL")
+    else:
+        cursor.execute("SELECT * FROM exercises WHERE username = ?", (username,))
+    exercises_rows = cursor.fetchall()
+    exercises: list[Exercise] = []
+    for exercises_row in exercises_rows:
+        exercises.append(convert_exercises_row_to_exercise(cursor, exercises_row))
+    return exercises
+
+
+@app.get("/users/me/exercises")
+def get_user_exercises(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+    conn: Connection = Depends(get_db)
+    ):
+    cursor: Cursor = conn.cursor()
+    return get_exercises(cursor, current_user.username)
+
+
+@app.get("/defaults/muscles")
+def get_muscles(conn: Connection = Depends(get_db)):
+    cursor: Cursor = conn.cursor()
+    cursor.execute("SELECT * FROM muscles")
+    muscles_rows = cursor.fetchall()
+    muscles: list[str] = []
+    for muscles_row in muscles_rows:
+        muscles.append(muscles_row["name"])
+
+    return muscles
+
+
+@app.get("/defaults/exercises")
+def get_default_exercises(conn: Connection = Depends(get_db)):
+    cursor: Cursor = conn.cursor()
+    return get_exercises(cursor)
 
 
 @app.get("/")
