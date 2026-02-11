aa2389f95a0e6acf0b4bbfe8f108cf198e843b8d
DrewAMSD <drewalgismiller2@gmail.com>
2026-02-10 23:45:00 -0800

    routes init

--- lifting-log-web/src/index.tsx
+++ lifting-log-web/src/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router';
 import './index.css';
 import App from './App';
+import Home from "./Routes/Home"
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+    </Routes>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
