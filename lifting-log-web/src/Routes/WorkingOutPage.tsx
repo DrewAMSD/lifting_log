ec342fb1e3b85a64a70c27da7130e0a363718392
DrewAMSD <drewalgismiller2@gmail.com>
2026-03-02 03:02:22 -0800

    initial workout state on redirect

--- lifting-log-web/src/Routes/WorkingOutPage.tsx
+++ lifting-log-web/src/Routes/WorkingOutPage.tsx
@@ -14,17 +14,20 @@ type WorkoutStats = {
     }
 }
 
-type Set_Entry = {
+type SetEntry = {
+    previous?: string
     weight?: number
     reps?: number
     time?: string
+    placeholder?: string
 }
 
-type Exercise_Entry = {
+type ExerciseEntry = {
     exercise_id: number
     exercise_name?: string
     description?: string
-    set_entries: Array<Set_Entry>
+    routine_note?: string
+    set_entries: Array<SetEntry>
 }
 
 type Workout = {
@@ -35,7 +38,7 @@ type Workout = {
     start_time: string
     duration: string
     stats?: WorkoutStats
-    exercise_entries: Array<Exercise_Entry>
+    exercise_entries: Array<ExerciseEntry>
 }
 
 const WorkingOutPage = (): JSX.Element => {
@@ -43,23 +46,63 @@ const WorkingOutPage = (): JSX.Element => {
     const [isLoading, setIsLoading] = useState<boolean>(true);
     const [workoutState, setWorkoutState] = useState<Workout>({} as Workout);
 
+    // TODO: add exercise select with fetch exercises on initial page redirect
     useEffect(() => {
         const workoutStateString: string | null = localStorage.getItem("workoutState");
+
         if (workoutStateString) {
             const currentWorkoutState: Workout = JSON.parse(workoutStateString) as Workout;
             setWorkoutState(currentWorkoutState);
         }
         else {
+            const currentDate: Date = new Date();
+            const hours: number = currentDate.getHours();
+            const minutes: number = currentDate.getMinutes();
+            const seconds: number = currentDate.getSeconds();
+            const currentTime: string = hours.toString().padStart(2, "0")+":"+minutes.toString().padStart(2, "0")+":"+seconds.toString().padStart(2, "0");
+
             const workoutTemplateString: string | null = localStorage.getItem("templateToUse");
             if (workoutTemplateString) {
                 const workoutTemplateToUse: WorkoutTemplate = JSON.parse(workoutTemplateString) as WorkoutTemplate;
-                // TODO: convert template into new workout object
+                
+                const workoutStateFromTemplate: Workout = {
+                    name: workoutTemplateToUse.name,
+                    date: currentDate.getFullYear(),
+                    start_time: currentTime,
+                    duration: "",
+                    exercise_entries: workoutTemplateToUse.exercise_templates.map((exerciseTemplate) => {             
+                        const exerciseEntry: ExerciseEntry = {
+                            exercise_id: exerciseTemplate.exercise_id,
+                            exercise_name: exerciseTemplate.exercise_name,
+                            description: "",
+                            routine_note: exerciseTemplate.routine_note,
+                            set_entries: exerciseTemplate.set_templates.map((setTemplate) => {
+                                // const isReps: boolean = exercise.reps && !exercise.weight;
+                                // const isRepRange: boolean = exercise.reps && exercise.weight;
+                                // const isTime: boolean = exercise.time;
+                                
+                                const setEntry: SetEntry = {
+                                    // TODO: update fields after implementing exercise fetch + grabbing previous exercises
+                                    previous: "",
+                                    placeholder: ""
+                                };
+                                // if (isReps) {
+                                //     setEntry.placeholder = setTemplate.reps ? setTemplate.reps.toString() : "";
+                                // }
+                                // else if (isRepRange) {
+                                //     setEntry.placeholder = (setTemplate.rep_range_start !== undefined && setTemplate.rep_range_end !== undefined) ? (setTemplate.rep_range_start.toString()+"-"+setTemplate.rep_range_end.toString()) : "";
+                                // }
+                                // else if (isTime) {
+                                //     setEntry.placeholder = setTemplate.time ? setTemplate.time : "";
+                                // }
+                                return setEntry;
+                            })
+                        }
+                        return exerciseEntry;
+                    })
+                }
+                setWorkoutState(workoutStateFromTemplate);
             } else {
-                const currentDate: Date = new Date();
-                const hours: number = currentDate.getHours();
-                const minutes: number = currentDate.getMinutes();
-                const seconds: number = currentDate.getSeconds();
-                const currentTime: string = hours.toString().padStart(2, "0")+":"+minutes.toString().padStart(2, "0")+":"+seconds.toString().padStart(2, "0");
                 const emptyWorkout: Workout = {
                     name: "",
                     date: currentDate.getFullYear(),
@@ -67,15 +110,25 @@ const WorkingOutPage = (): JSX.Element => {
                     duration: "",
                     exercise_entries: []
                 };
+                setWorkoutState(emptyWorkout);
             }
         }
 
         setIsLoading(false);
     }, []);
 
+    // TODO: add button for discard workout where it clears templateToUse and workoutState in localStorage
     return (
         <div className="route-container">
-            Working Out Page
+            {
+                isLoading ? (
+                    <div>Loading...</div>
+                ) : (
+                    <p>
+                        Working out
+                    </p>
+                )
+            }
         </div>
     )
 };
